c5d0b454b9b6ea383f1e62822e0abf86d6b71c08
Lam Tan <nhat.tan93@yahoo.com>
2017-08-23 13:35:32 +0200

    Tp02 Vues OK

--- 05-angularjs/src/index.js
+++ 05-angularjs/src/index.js
@@ -1,8 +1,10 @@
+
 console.log('Bonjour App');
 
 // récupération de la page HTML ./tp01/expressions.html à l'aide de Webpack
 import tplTp01 from './tp01/expressions.html'
+import tplTp02 from './tp02/vues.html'
 // insertion du code HTML dans le corps de la page principale
 document.querySelector('body').innerHTML = [tplTp01].join('<hr>');
-
+document.querySelector('body').innerHTML = [tplTp02].join('<hr>');
 import angular from 'angular'
