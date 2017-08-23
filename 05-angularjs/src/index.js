7cdfd41c597ccc518f2d98cb2319eb5a9f3fe6a6
Lam Tan <nhat.tan93@yahoo.com>
2017-08-23 16:14:07 +0200

    ok

--- 05-angularjs/src/index.js
+++ 05-angularjs/src/index.js
@@ -5,14 +5,12 @@ import angular from 'angular'
 import tplTp01 from './tp01/expressions.html'
 import tplTp02 from './tp02/vues.html'
 import tplTp03 from './tp03/carrousel.html'
+import CarrouselCtrl from './tp03/carrousel.controller'
 
 // insertion du code HTML dans le corps de la page principale
 document.querySelector('body').innerHTML = [tplTp01].join('<hr>');
 document.querySelector('body').innerHTML = [tplTp02].join('<hr>');
 document.querySelector('body').innerHTML = [tplTp03].join('<hr>');
 
-
-
-import CarrouselCtrl from './tp03/carrousel.controller'
 angular.module('tripApp', []) 
 .controller(CarrouselCtrl.name, CarrouselCtrl)
